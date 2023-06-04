fd2aed685915347b3d56fe223e11569841ae329e
Julia Abramenko <julia.abramenko@protonmail.com>
2023-06-04 15:25:07 +0200

    Defined Component modules and function for AppComponent

---
+++ app/src/main/java/com/example/moviesapp/presentation/dependencyinjection/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.example.moviesapp.presentation.dependencyinjection
+
+import dagger.Component
+import okhttp3.Cache
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [
+    AppModule::class,
+    CacheDataModule::class,
+    DatabaseModule::class,
+    LocalDataModule::class,
+    MovieModule::class,
+    NetModule::class,
+    RemoteDataModule::class,
+    RepositoryModule::class,
+    UseCaseModule::class
+])
+interface AppComponent {
+
+    fun movieSubComponent(): MovieSubComponent.Factory
+}
